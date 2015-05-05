075de6271e2eea479e144bfa774cb37b843c31ae
Danilo Trindade <chapolin@gmail.com>
2015-05-04 23:12:57 -0300

    Change port application

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mongo = null;
+var port = process.env.PORT || 8080;
 
 //Retrieve
 var MongoClient = require('mongodb').MongoClient;
@@ -39,7 +40,7 @@ app.get('/search', function(request, response) {
 	}
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
