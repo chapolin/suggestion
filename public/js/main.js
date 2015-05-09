5e13a80cd70f7051131b69e606c1f57fb821d9fc
Carlos Vieira <carlos.vieira@outlook.com>
2015-05-09 15:25:41 -0300

    Change Pallete for first test

--- public/js/main.js
+++ public/js/main.js
@@ -85,5 +85,5 @@ $(document).ready(function () {
     	postData.place = $('input[name="_place"]').val();
     });
     
-    $('.money').mask('000.000.000.000.000,00', {reverse: true});
+    $('.money').mask("#.##0,00", {reverse: true});
 });
